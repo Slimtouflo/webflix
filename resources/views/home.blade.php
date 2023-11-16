104f01301c8b04cff194b67ed1ea56d4801b9ea4
Florent Creton <slimtouflo@gmail.com>
2023-11-16 11:28:45 +0100

    layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts/app')
+
+{{-- On prend le contenu de layouts/app.blade.php --}}
+{{-- Le contenu de la section content va prendre la place du yield content --}}
+@section('content')
+    <h1>Hello {{ $name }}</h1>
+
+    <ul>
+    @foreach ($games as $game)
+        <li>{{ $game }}</li>
+    @endforeach
+    </ul>
+@endsection
+
