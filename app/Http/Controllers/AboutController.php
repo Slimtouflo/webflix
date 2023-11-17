9753008da8d457acc7024b1792f01be6a7ff323f
Florent Creton <slimtouflo@gmail.com>
2023-11-17 10:30:24 +0100

    creation table movie

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,28 +4,32 @@ namespace App\Http\Controllers;
 
 class AboutController extends Controller
 {
+    public $team = [
+        [
+            'name'  => 'Fiorella',
+            'job'   => 'CEO',
+            'image' => 'https://i.pravatar.cc/100?u=remi'
+        ],
+        [
+            'name'  => 'Toto',
+            'job'   => 'CTO',
+            'image' => 'https://i.pravatar.cc/100?u=patrick'
+        ],
+    ];
+
     public function index()
     {
         return view('a-propos', [
             'title' => 'Les devs',
-            'team'  => [
-                [
-                    'name'  => 'Fiorella',
-                    'job'   => 'CEO',
-                    'image' => 'https://i.pravatar.cc/100?u=remi'
-                ],
-                [
-                    'name'  => 'Toto',
-                    'job'   => 'CTO',
-                    'image' => 'https://i.pravatar.cc/100?u=patrick'
-                ],
-            ]
+            'team'  => $this->team,
         ]);
     }
 
     public function show($user)
     {
-        if (! in_array($user, ['Fiorella', 'Toto'])) {
+        $users = collect($this->team)->pluck('name')->all();
+
+        if (! in_array($user, $users)) {
             abort(404); // Renvoie une 404
         }
 
